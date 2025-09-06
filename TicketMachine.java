85f846d57f577a37335cf7e753f5c63e3928eb3d
Varshiha <varshihaa@icloud.com>
2025-09-05 20:39:04 -0400

    question 64: I am not sure how to create a new project so I left it as it is.

--- TicketMachine.java
+++ TicketMachine.java
@@ -17,6 +17,11 @@ public class TicketMachine
     private int balance;
     // The total amount of money collected by this machine.
     private int total;
+     private boolean discountOn;
+   public void activateDiscount()
+   {
+      discountOn = true;   
+   }
     
     
     /**
@@ -66,24 +71,34 @@ public class TicketMachine
      */
     public void printTicket()
     {
-        int amountLeftToPay = price - balance;
+        int originalPrice = price;
+        if (discountOn)
+        {
+            originalPrice = price /2;
+            
+        }
+       
+        
+        int amountLeftToPay = originalPrice - balance;
         if(amountLeftToPay <= 0) {
             // Simulate the printing of a ticket.
             System.out.println("##################");
             System.out.println("# The BlueJ Line");
             System.out.println("# Ticket");
-            System.out.println("# " + price + " cents.");
+            System.out.println("# " + originalPrice + " cents.");
             System.out.println("##################");
             System.out.println();
 
             // Update the total collected with the price.
-            total = total + price;
+            total = total + originalPrice;
             // Reduce the balance by the price.
-            balance = balance - price;
+            balance = balance - originalPrice;
         }
         else {
             System.out.printf("You must insert " + amountLeftToPay + " cents.") ;
         }
+        
+        discountOn = false;
     }
 
     /**
